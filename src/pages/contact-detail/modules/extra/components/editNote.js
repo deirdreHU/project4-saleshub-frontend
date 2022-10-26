b346ca79e9a6edac3a0991cdd801e87bc4da4f25
Deirdre Hu <deirdre.hu@edoovo.com>
2022-10-27 01:24:03 +0800

    feat:edit note

---
+++ src/pages/contact-detail/modules/extra/components/editNote.js
@@ -0,0 +1,78 @@
+import {Button, TextField} from "@mui/material";
+import {DrawerForm} from "../../../../../components/drawerForm/DrawerForm";
+import {Form} from "antd";
+import {useParams} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {authedRequest} from "../../../../../http";
+import {REFRESH_CONTACT} from "../../../../contacts/store";
+
+export const EditNote = ({note, open, setOpen}) => {
+    const {contact_id} = useParams();
+    const dispatch = useDispatch();
+    
+    const handleSubmit = async (values) => {
+        await authedRequest.put(`/api/notes/${note.noteId}`, {
+        content: values.content,
+        contact: Number(contact_id)
+        });
+        setOpen(false);
+        dispatch(REFRESH_CONTACT(contact_id));
+    }
+    if (!note) {
+        return <></>;
+    }
+    return (
+        <div>
+
+            <DrawerForm open={open} onClose={() => setOpen(false)}>
+
+                <div className={'p-3'}>
+
+                    <h3>Update Note</h3>
+
+                    <Form
+                        initialValues={note}
+                        className={'mt-3'}
+                        onFinish={handleSubmit}>
+
+                        <Form.Item
+                            name={'content'}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter the note content..."
+                                }
+                            ]}>
+
+                            <TextField
+                                multiline
+                                rows={5}
+                                fullWidth
+                                variant={'outlined'}
+                                placeholder={'Enter your note here...'}
+                                label={'Contact note'}/>
+
+                        </Form.Item>
+
+                        <Form.Item>
+
+                            <Button
+                                type={'submit'}
+                                className={'me-2'}
+                                variant={'contained'}>Update</Button>
+                            <Button
+                                onClick={() => setOpen(false)}
+                                variant={'contained'}
+                                color={'inherit'}>Cancel</Button>
+
+                        </Form.Item>
+
+                    </Form>
+                    
+                </div>
+
+            </DrawerForm>
+            
+        </div>
+    )
+    }
--- src/pages/contact-detail/modules/extra/components/notes.js
+++ src/pages/contact-detail/modules/extra/components/notes.js
@@ -1,25 +1,65 @@
-import {Button} from "@mui/material";
 import MenuAction from "../../../../../components/menuAction/MenuAction";
+import {CreateNote} from "./createNote";
+import {EditNote} from "./editNote";
+
+import {useState} from "react";
+
+import {authedRequest} from "../../../../../http";
+import {useDispatch} from "react-redux";
+import {REFRESH_CONTACT} from "../../../../home/store";
+import {useParams} from "react-router-dom";
+import {Modal} from 'antd';
 
 function Notes({notes}) {
+    const [note, setNote] = useState(null);
+    const [open, setOpen] = useState(false);
+    const dispatch = useDispatch();
+    
+    const {contact_id} = useParams();
+    const handleDelete = async (noteId) => {
+
+        Modal.confirm({
+            content: 'Do you want to delete this note?',
+            onOk: async () => {
+                await authedRequest.delete(`/api/notes/${noteId}`);
+                dispatch(REFRESH_CONTACT(contact_id))
+            }
+        })
+
+    }
 
     return (
         <div >
 
             <div className={'text-end'}>
-                <Button variant={'contained'}>Create Note</Button>
+                <CreateNote />
             </div>
 
+            <EditNote note={note} open={open} setOpen={setOpen} />
+
             <div className={'mt-3'}>
+
                 {notes.map(note => {
+
                     return (
-                        <div key={note.noteId} className={'card p-3 w-100'}>
+
+                        <div key={note.noteId} className={'card p-3 w-100 mt-2'}>
 
                             <div className={'d-flex justify-content-between'}>
+                        
                                 <strong>Note</strong>
-                                <MenuAction />
-                            </div>
+                        
+                                <MenuAction
+                                    onDelete={() => {
+                                        handleDelete(note.noteId)
+                                    }}
+                                    onEdit={() => {
+                                        setNote(note);
+                                        setOpen(true);
+                                    }}/>
 
+                            </div>
+                        
                             <p className={'mt-2'}>{note.content}</p>
 
                             <span className={'text-primary'} role={'button'}>Add Comment</span>
@@ -31,5 +71,4 @@ function Notes({notes}) {
         </div>
     )
 }
-
 export default Notes;
