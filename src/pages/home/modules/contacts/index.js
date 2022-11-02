6c37c78e0795754fe4c40d56e507f3603ea62236
Deirdre Hu <deirdre.hu@edoovo.com>
2022-11-02 19:14:47 +0800

    add function to change the columns

--- src/pages/home/modules/contacts/index.js
+++ src/pages/home/modules/contacts/index.js
@@ -8,72 +8,80 @@ import moment from 'moment';
 import SearchSelector from "../../../../components/searchSelector/SearchSelector";
 import {useStaff} from "../../../../hooks/useStaff";
 import {useNavigate} from "react-router-dom";
+import ReactDragListView from 'react-drag-listview'
 
-const TableColumns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a, b) => a.name.localeCompare(b.name),
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-        sorter: (a, b) => a.email.localeCompare(b.email),
-    },
-    {
-        title: 'Phone',
-        dataIndex: 'phone',
-        key: 'phone',
-        sorter: (a, b) => a.phone.localeCompare(b.phone),
-    },
-    {
-        title: 'Assigned To',
-        dataIndex: 'User',
-        key: 'User',
-        sorter: (a, b) => a.User.username.localeCompare(b.User.username),
-        render: (_, record) => {
-            return record.User.username
-        }
-    },
-    {
-        title: 'Lifecycle Stage',
-        dataIndex: 'lifeCycleStage',
-        key: 'lifeCycleStage',
-        sorter: (a, b) => a.lifeCycleStage.localeCompare(b.lifeCycleStage),
-    },
-    {
-        title: 'Country',
-        dataIndex: 'country',
-        key: 'country',
-        sorter: (a, b) => a.country.localeCompare(b.country),
-    },
-    {
-        title: 'Created On',
-        dataIndex: 'createdAt',
-        key: 'createdAt',
-        render: (record) => {
-            return moment(record.createdAt).format('lll');
+export const Contacts = (props) =>  {
+    const { navType, isEqualRedd } = props;
+    const [_TableColumns, _setColumns] = useState([]);
+
+    let TableColumns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
         },
-        sorter: (a, b) => new Date(a).getTime() - new Date(b).getTime()
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action',
-        render: () => {
-            return (
-                <div>
-                    <Button color={'warning'}>DELETE</Button>
-                </div>
-            )
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            sorter: (a, b) => a.email.localeCompare(b.email),
+        },
+        {
+            title: 'Phone',
+            dataIndex: 'phone',
+            key: 'phone',
+            sorter: (a, b) => a.phone.localeCompare(b.phone),
+        },
+        {
+            title: 'Assigned To',
+            dataIndex: 'User',
+            key: 'User',
+            sorter: (a, b) => a.User.username.localeCompare(b.User.username),
+            render: (_, record) => {
+                return record.User.username
+            }
+        },
+        {
+            title: 'Lifecycle Stage',
+            dataIndex: 'lifeCycleStage',
+            key: 'lifeCycleStage',
+            sorter: (a, b) => a.lifeCycleStage.localeCompare(b.lifeCycleStage),
+        },
+        {
+            title: 'Country',
+            dataIndex: 'country',
+            key: 'country',
+            sorter: (a, b) => a.country.localeCompare(b.country),
+        },
+        {
+            title: 'Created On',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            render: (record) => {
+                return moment(record.createdAt).format('lll');
+            },
+            sorter: (a, b) => new Date(a).getTime() - new Date(b).getTime()
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            key: 'action',
+            render: () => {
+                return (
+                    <div>
+                        <Button color={'warning'}>DELETE</Button>
+                    </div>
+                )
+            }
         }
-    }
-];
+    ];
 
+    useEffect(() => {
+        _setColumns(TableColumns);
+        // eslint-disable-next-line
+    }, [navType, isEqualRedd]);
 
-function Contacts() {
     const contacts = useSelector(state => state.home.contacts);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -106,8 +114,23 @@ function Contacts() {
         // eslint-disable-next-line 
     }, [lifeCycleStage]);
 
+    const dragProps = {
+        onDragEnd(fromIndex, toIndex) {
+            const columns = [..._TableColumns];
+            const item = columns.splice(fromIndex, 1)[0];
+            columns.splice(toIndex, 0, item);
+            console.log(columns)
+            // TableColumns = columns
+            // TableColumns = [...columns]
+            // TableColumns = columns
+            _setColumns(columns);
+        },
+        nodeSelector: "th"
+    };
+
     return (
-        <div>
+
+        <>
 
             <div className={'d-flex'}>
                 <div variant={'h6'} className={'me-auto'}>Contacts</div>
@@ -148,18 +171,21 @@ function Contacts() {
 
             </div>
 
-            <Table
-                onRow={record => {
-                    return {
-                        onClick: e => {
-                        navigate(`/contacts/${record.contactId}`)
+            <ReactDragListView.DragColumn {...dragProps}>
+
+                <Table
+                    onRow={record => {
+                        return {
+                            onClick: e => {
+                                navigate(`/contacts/${record.contactId}`)
+                            }
                         }
-                    }
-                }}
+                    }}
+                    className={'mt-4'} columns={_TableColumns} dataSource={contacts}/>
 
-                className={'mt-4'} columns={TableColumns} dataSource={contacts}/>
+            </ReactDragListView.DragColumn>
 
-        </div>
+        </>
 
     )
 }
--- src/pages/home/modules/deals/components/DealsTable.jsx
+++ src/pages/home/modules/deals/components/DealsTable.jsx
@@ -2,73 +2,83 @@ import moment from "moment";
 import {Table} from "antd";
 import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+import React, { useEffect, useState } from 'react';
 import {REFRESH_DEALS} from "../../../store";
+import ReactDragListView from 'react-drag-listview'
 
-const TableColumns = [
-    {
-        title: 'DEAL NAME',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a, b) => a.name.localeCompare(b.name),
-    },
+export const DealsTable = (props) =>  {
+    const { navType, isEqualRedd } = props;
+    const [_TableColumns, _setColumns] = useState([]);
+    
+    //defalut table columns
+    let TableColumns = [
+        {
+            title: 'DEAL NAME',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+        },
 
-    {
-        title: 'DEAL Amount',
-        dataIndex: 'amount',
-        key: 'amount',
-        sorter: (a, b) => a.amount - b.amount,
-    },
+        {
+            title: 'DEAL Amount',
+            dataIndex: 'amount',
+            key: 'amount',
+            sorter: (a, b) => a.amount - b.amount,
+        },
 
-    {
-        title: 'DEAL STAGE',
-        dataIndex: 'dealStage',
-        key: 'dealStage',
-        sorter: (a, b) => a.dealStage.localeCompare(b.dealStage),
-    },
+        {
+            title: 'DEAL STAGE',
+            dataIndex: 'dealStage',
+            key: 'dealStage',
+            sorter: (a, b) => a.dealStage.localeCompare(b.dealStage),
+        },
 
-    {
-        title: 'ASSIGNED TO',
-        dataIndex: 'User',
-        key: 'User',
-        sorter: (a, b) => a.User.username.localeCompare(b.User.username),
-        render: (_, record) => {
-            return record.User.username
-        }
-    },
+        {
+            title: 'ASSIGNED TO',
+            dataIndex: 'User',
+            key: 'User',
+            sorter: (a, b) => a.User.username.localeCompare(b.User.username),
+            render: (_, record) => {
+                return record.User.username
+            }
+        },
 
-    {
-        title: 'CREATED ON',
-        dataIndex: 'createdAt',
-        key: 'createdAt',
-        sorter: (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
-        render: (_, record) => {
-            return moment(new Date(record.createdAt)).format('lll')
-        }
-    },
+        {
+            title: 'CREATED ON',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            sorter: (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+            render: (_, record) => {
+                return moment(new Date(record.createdAt)).format('lll')
+            }
+        },
 
-    {
-        title: 'CLOSED ON',
-        dataIndex: 'closeDate',
-        key: 'closeDate',
-        sorter: (a, b) => new Date(a.closeDate).getTime() - new Date(b.closeDate).getTime(),
-        render: (_, record) => {
-            return record.closeDate ? moment(new Date(record.closeDate)).format('lll') : '-'
-        }
-    },
+        {
+            title: 'CLOSED ON',
+            dataIndex: 'closeDate',
+            key: 'closeDate',
+            sorter: (a, b) => new Date(a.closeDate).getTime() - new Date(b.closeDate).getTime(),
+            render: (_, record) => {
+                return record.closeDate ? moment(new Date(record.closeDate)).format('lll') : '-'
+            }
+        },
 
-    {
-        title: 'ASSOCIATED CONTACT',
-        dataIndex: 'Contact',
-        key: 'Contact',
-        sorter: (a, b) => a.Contact.name.localeCompare(b.Contact.name),
-        render: (_, record) => {
-            return record.Contact.name;
-        }
-    },
-];
+        {
+            title: 'ASSOCIATED CONTACT',
+            dataIndex: 'Contact',
+            key: 'Contact',
+            sorter: (a, b) => a.Contact.name.localeCompare(b.Contact.name),
+            render: (_, record) => {
+                return record.Contact.name;
+            }
+        },
+    ];
 
-export const DealsTable = () => {
+
+    useEffect(() => {
+        _setColumns(TableColumns);
+        // eslint-disable-next-line
+    }, [navType, isEqualRedd]);
 
     const deals = useSelector(state => state.home.deals);
     const navigate = useNavigate();
@@ -76,18 +86,36 @@ export const DealsTable = () => {
 
     useEffect(() => {
         dispatch(REFRESH_DEALS());
-        // eslint-disable-next-line 
+        // eslint-disable-next-line
     }, []);
 
+    // const that = this;
+    const dragProps = {
+        onDragEnd(fromIndex, toIndex) {
+            const columns = [..._TableColumns];
+            const item = columns.splice(fromIndex, 1)[0];
+            columns.splice(toIndex, 0, item);
+            console.log(columns)
+            // TableColumns = columns
+            // TableColumns = [...columns]
+            // TableColumns = columns
+            _setColumns(columns);
+        },
+        nodeSelector: "th"
+    };
+
+
     return (
-        <Table
-            onRow={record => {
-                return {
-                    onClick: e => {
-                        navigate(`/deals/${record.dealId}`)
+        <ReactDragListView.DragColumn {...dragProps}>
+            <Table
+                onRow={record => {
+                    return {
+                        onClick: e => {
+                            navigate(`/deals/${record.dealId}`)
+                        }
                     }
-                }
-            }}
-        className={'mt-4'} columns={TableColumns} dataSource={deals}/>
+                }}
+            className={'mt-4'} columns={_TableColumns} dataSource={deals}/>
+        </ReactDragListView.DragColumn>
     )
 }
